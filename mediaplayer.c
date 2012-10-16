854b2d15e46487385c2dbcf4ac9e509fa2478624
Morten Minde Neergaard <mneergaa@cisco.com>
2012-10-16 16:06:17 +0200

    Refactor everything, make gapless work (?), add proper-ish playlist support, and go back to GStreamer0.10

--- mediaplayer.c
+++ mediaplayer.c
@@ -1,56 +1,122 @@
 #include <stdlib.h>
+#include <string.h>
 #include <gst/gst.h>
 #include <glib.h>
 
 gboolean empamp_verbose = FALSE;
+int playlist_index = -1, playlist_size = 0;
+char **playlist = NULL;
 
-static gboolean bus_call (GstBus *bus, GstMessage *msg, gpointer data)
+static void handle_eos (GstBus *bus, GstMessage *msg, GMainLoop *loop)
 {
-	GMainLoop *loop = (GMainLoop *) data;
+	if (empamp_verbose)
+		g_print ("End of stream\n");
+	g_main_loop_quit (loop);
+}
 
-	switch (GST_MESSAGE_TYPE (msg)) {
+static void handle_error (GstBus *bus, GstMessage *msg, GMainLoop *loop)
+{
+	gchar  *debug;
+	GError *error;
 
-		case GST_MESSAGE_EOS:
-			g_print ("End of stream\n");
-			g_main_loop_quit (loop);
-			break;
+	gst_message_parse_error (msg, &error, &debug);
+	g_printerr ("ERROR: %s (%s)\n", error->message, (debug) ? debug : "no details");
+	g_free (debug);
+	g_error_free (error);
 
-		case GST_MESSAGE_ERROR: {
-			gchar  *debug;
-			GError *error;
+	g_main_loop_quit (loop);
+}
 
-			gst_message_parse_error (msg, &error, &debug);
-			g_free (debug);
+static void handle_warning (GstBus *bus, GstMessage *msg, GMainLoop *loop)
+{
+	gchar  *debug;
+	GError *error;
 
-			g_printerr ("Error: %s\n", error->message);
-			g_error_free (error);
+	gst_message_parse_warning (msg, &error, &debug);
+	g_printerr ("WARNING: %s (%s)\n", error->message, (debug) ? debug : "no details");
+	g_free (debug);
+	g_error_free (error);
+}
 
-			g_main_loop_quit (loop);
-			break;
+static void handle_clock_loss (GstBus *bus, GstMessage *msg, GstElement *playbin)
+{
+	GstClock *clock;
+
+	gst_message_parse_clock_lost (msg, &clock);
+	g_print ("CLOCK LOST: %s\n", GST_OBJECT_NAME (clock));
+
+	gst_element_set_state (playbin, GST_STATE_PAUSED);
+	gst_element_set_state (playbin, GST_STATE_PLAYING);
+}
+
+static gchar * try_force_to_uri (gchar *path)
+{
+	gchar *temp = NULL;
+	temp = g_uri_parse_scheme (path);
+	// If the path isn't a URI, try to make it into one
+	if (temp == NULL) {
+		if (! g_path_is_absolute (path)) {
+			temp = g_get_current_dir ();
+			path = g_build_filename (temp, path, NULL);
 		}
-		default:
-			break;
+		temp = g_filename_to_uri (path, NULL, NULL);
+		// If the string can't be made into a URI, return the string
+		if (temp != NULL)
+			path = g_strdup (temp);
 	}
+	g_free (temp);
+	return path;
+}
+
+static void playlist_set_previous (GstElement *playbin)
+{
+	playlist_index = (playlist_index - 1) % playlist_size;
+	g_object_set (G_OBJECT (playbin), "uri", playlist[playlist_index], NULL);
+}
+
+static void playlist_set_next (GstElement *playbin)
+{
+	playlist_index = (playlist_index + 1) % playlist_size;
+	if (empamp_verbose)
+		g_print ("Queueing file: %s\n", playlist[playlist_index]);
+	g_object_set (G_OBJECT (playbin), "uri", playlist[playlist_index], NULL);
+}
+
+static void toggle_play_pause (GstElement *playbin) {
+	GstState state;
+	gst_element_get_state (playbin, &state, NULL, GST_CLOCK_TIME_NONE);
+	if (state == GST_STATE_PLAYING) {
+		gst_element_set_state (playbin, GST_STATE_PAUSED);
+	} else {
+		gst_element_set_state (playbin, GST_STATE_PLAYING);
+	}
+}
+
+static gboolean print_progress (GstElement *playbin)
+{
+	gint64 duration = -1;
+	gint64 position = -1;
+	GstFormat format = GST_FORMAT_TIME;
+	gchar dur_str[32], pos_str[32];
+
+	if (gst_element_query_position (playbin, &format, &position) &&
+			position != -1) {
+		g_snprintf (pos_str, 32, "%" GST_TIME_FORMAT, GST_TIME_ARGS (position));
+	} else {
+		g_snprintf (pos_str, 32, "-:--:--.---------");
+	}
+
+	if (gst_element_query_duration (playbin, &format, &duration) &&
+			duration != -1) {
+		g_snprintf (dur_str, 32, "%" GST_TIME_FORMAT, GST_TIME_ARGS (duration));
+	} else {
+		g_snprintf (dur_str, 32, "-:--:--.---------");
+	}
+
+	g_print ("%s / %s\r", pos_str, dur_str);
 
 	return TRUE;
 }
-
-
-static void on_pad_added (GstElement *element, GstPad *pad, gpointer data)
-{
-	GstPad *sinkpad;
-	GstElement *decoder = (GstElement *) data;
-
-	/* We can now link this pad with the vorbis-decoder sink pad */
-	g_print ("Dynamic pad created.\n");
-
-	sinkpad = gst_element_get_static_pad (decoder, "sink");
-
-	gst_pad_link (pad, sinkpad);
-
-	gst_object_unref (sinkpad);
-}
-
 
 
 int main (int argc, char *argv[])
@@ -58,7 +124,7 @@ int main (int argc, char *argv[])
 
 	GMainLoop *loop;
 
-	GstElement *pipeline, *source, *decoder, *conv, *sink;
+	GstElement *playbin;
 	GstBus *bus;
 
 	GOptionContext *ctx;
@@ -66,6 +132,8 @@ int main (int argc, char *argv[])
 	GOptionEntry entries[] = {
 		{ "verbose", 'v', 0, G_OPTION_ARG_NONE, &empamp_verbose,
 			"Output verbosely", NULL },
+		{ G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_FILENAME_ARRAY, &playlist,
+			    "Filename(s)" },
 		{ NULL }
 	};
 
@@ -73,92 +141,75 @@ int main (int argc, char *argv[])
 	 *    * other GLib funtion, such as g_option_context_new() */
 	if (!g_thread_supported ())
 		g_thread_init (NULL);
+	gst_init (&argc, &argv);
 
 	ctx = g_option_context_new ("filename [more filenames] - play media files");
 	g_option_context_add_main_entries (ctx, entries, NULL);
 	g_option_context_add_group (ctx, gst_init_get_option_group ());
 	if (!g_option_context_parse (ctx, &argc, &argv, &err)) {
-		g_print ("%s\n", err->message);
+		g_printerr ("%s\n", err->message);
 		g_error_free (err);
 		return EXIT_FAILURE;
 	}
 
 	/* Initialisation */
-	gst_init (&argc, &argv);
 
 	loop = g_main_loop_new (NULL, FALSE);
 
+	playlist_size = g_strv_length (playlist);
+	for (int i = 0 ; i != playlist_size ; i++)
+		playlist[i] = try_force_to_uri (playlist[i]);
 
 	/* Check input arguments */
-	if (argc < 2) {
+	if (playlist_size < 1) {
 		g_printerr (g_option_context_get_help (ctx, TRUE, NULL));
 		return EXIT_FAILURE;
 	}
 
-
 	/* Create gstreamer elements */
-	pipeline = gst_pipeline_new ("audio-player");
-	source   = gst_element_factory_make ("filesrc",       "file-source");
-	decoder  = gst_element_factory_make ("decodebin",     "decoder");
+	playbin = gst_element_factory_make ("playbin2", "playbin");
 
-	conv     = gst_element_factory_make ("audioconvert",  "converter");
-	sink     = gst_element_factory_make ("autoaudiosink", "audio-output");
-
-	if (!pipeline || !source || !decoder || !conv || !sink) {
-		g_printerr ("One element could not be created. Exiting.\n");
+	if (!playbin) {
+		g_printerr ("Player could not be created. Exiting.\n");
 		return EXIT_FAILURE;
 	}
 
 	/* Set up the pipeline */
 
 	/* we add a message handler */
-	bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
-	gst_bus_add_watch (bus, bus_call, loop);
-	gst_object_unref (bus);
+	bus = gst_pipeline_get_bus (GST_PIPELINE (playbin));
+	gst_bus_add_signal_watch (bus);
 
-	/* we add all elements into the pipeline */
-	/* file-source | decodebin | converter | alsa-output */
-	gst_bin_add_many (GST_BIN (pipeline),
-			source, decoder, conv, sink, NULL);
-
-	/* we link the elements together */
-	/* file-source -> ogg-demuxer ~> vorbis-decoder -> converter -> alsa-output */
-	gst_element_link (source, decoder);
-	gst_element_link_many (conv, sink, NULL);
-	g_signal_connect (decoder, "pad-added", G_CALLBACK (on_pad_added), conv);
-
-	/* note that the decoder will be linked to the converter dynamically.
-	The reason is that Ogg may contain various streams (for example
-	audio and video). The source pad(s) will be created at run time,
-	by the demuxer when it detects the amount and nature of streams.
-	Therefore we connect a callback function which will be executed
-	when the "pad-added" is emitted.*/
-
-	for (int i = 1 ; i != argc ; i++) {
-
-		/* we set the input filename to the source element */
-		g_object_set (G_OBJECT (source), "location", argv[i], NULL);
+	g_signal_connect (bus, "message::eos", G_CALLBACK (handle_eos), loop);
+	g_signal_connect (bus, "message::error", G_CALLBACK (handle_error), loop);
+	g_signal_connect (bus, "message::warning", G_CALLBACK (handle_warning), NULL);
+	// TODO Does this ever happen?
+	g_signal_connect (bus, "message::clock-lost", G_CALLBACK (handle_clock_loss), playbin);
 
 
-		/* Set the pipeline to "playing" state*/
-		g_print ("Now playing: %s\n", argv[i]);
-		gst_element_set_state (pipeline, GST_STATE_PLAYING);
+	/* Handle adding the next element in the playlist. This makes us gapless */
+	g_signal_connect (playbin, "about-to-finish", G_CALLBACK (playlist_set_next), NULL);
+
+	playlist_set_next (playbin);
+	toggle_play_pause (playbin);
+
+	g_timeout_add (33, (GSourceFunc) print_progress, playbin);
+
+	/* Iterate */
+	if (empamp_verbose)
+		g_print ("Running...\n");
+	g_main_loop_run (loop);
 
 
-		/* Iterate */
-		if (empamp_verbose)
-			g_print ("Running...\n");
-		g_main_loop_run (loop);
-
-
-		/* Out of the main loop, clean up nicely */
-		g_print ("Returned, stopping playback\n");
-		gst_element_set_state (pipeline, GST_STATE_NULL);
-	}
+	/* Out of the main loop, clean up nicely */
+	g_print ("Quitting...\n");
+	gst_element_set_state (playbin, GST_STATE_NULL);
 
 	if (empamp_verbose)
 		g_print ("Deleting pipeline\n");
-	gst_object_unref (GST_OBJECT (pipeline));
+	gst_object_unref (playbin);
+	gst_object_unref (bus);
+	g_main_loop_unref (loop);
 
 	return EXIT_SUCCESS;
 }
